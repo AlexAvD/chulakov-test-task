dfa3e9454dba781f0d6e812a441fdf888b66d5fa
Alex Aganov <alex.aganow@yandex.ru>
2020-01-30 01:48:55 +0300

    добавил router

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import store from './redux/store'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom' 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Route paht="/" component={App} />
+    </Router> 
+  </Provider>, 
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
